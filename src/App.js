a3f9e721dddec593b5efab9f6d7798b86df6dcc8
AswiniSureddy <aswini.sureddy@gmail.com>
2023-08-22 12:25:15 +0200

    hackers basic files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [stories, setStories] = useState([]);
+
+  useEffect(() => {
+    fetchTopStories();
+  }, []);
+
+  async function fetchTopStories() {
+    const response = await fetch('https://hacker-news.firebaseio.com/v0/topstories.json');
+    const storyIds = await response.json();
+    
+    const storyPromises = storyIds.slice(0, 10).map(async (id) => {
+      const storyResponse = await fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json`);
+      return storyResponse.json();
+    });
+
+    const storiesData = await Promise.all(storyPromises);
+    const sortedStories = storiesData.sort((a, b) => a.score - b.score);
+
+    setStories(sortedStories);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hacker News Stories</h1>
+      <ul className="stories-list">
+        {stories.map((story) => (
+          <li key={story.id} className="story">
+            <h2>{story.title}</h2>
+            <p>URL: <a href={story.url} target="_blank" rel="noopener noreferrer">{story.url}</a></p>
+            <p>Timestamp: {new Date(story.time * 1000).toLocaleString()}</p>
+            <p>Score: {story.score}</p>
+            <p>Author ID: {story.by}</p>
+            <p>Author Karma Score: {story.karma}</p>
+            <img src={require('./dummy-image.jpeg')} alt="Dummy Image" />
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
